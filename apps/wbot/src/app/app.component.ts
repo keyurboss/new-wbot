f6b29ba229f68f67740bf8955d4c997cdfda4971
Keyur Shah <keyurshah3939@gmail.com>
2021-09-27 00:20:52 +0530

    initial commit

---
+++ apps/wbot/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-wbot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'wbot';
+}
